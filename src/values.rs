5c324e27fe204ad6631c9ce35b2d1d26e0feffc1
Tomaka17 <pierre.krieger1708@gmail.com>
2014-06-24 16:17:45 +0200

    Add tests for values pushing/reading

--- src/values.rs
+++ src/values.rs
@@ -137,11 +137,116 @@ impl Index for bool {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn readwrite_ints() {
+    fn read_ints() {
+        let mut lua = super::super::Lua::new();
+
+        lua.set("a", 2).unwrap();
+
+        let x: int = lua.get("a").unwrap();
+        assert_eq!(x, 2);
+
+        let y: i8 = lua.get("a").unwrap();
+        assert_eq!(y, 2);
+
+        let z: i16 = lua.get("a").unwrap();
+        assert_eq!(z, 2);
+
+        let w: i32 = lua.get("a").unwrap();
+        assert_eq!(w, 2);
+
+        let a: uint = lua.get("a").unwrap();
+        assert_eq!(a, 2);
+
+        let b: u8 = lua.get("a").unwrap();
+        assert_eq!(b, 2);
+
+        let c: u16 = lua.get("a").unwrap();
+        assert_eq!(c, 2);
+
+        let d: u32 = lua.get("a").unwrap();
+        assert_eq!(d, 2);
+    }
+
+    #[test]
+    fn write_ints() {
+        // TODO: 
+
         let mut lua = super::super::Lua::new();
 
         lua.set("a", 2).unwrap();
         let x: int = lua.get("a").unwrap();
         assert_eq!(x, 2);
+    }
+
+    #[test]
+    fn readwrite_floats() {
+        let mut lua = super::super::Lua::new();
+
+        lua.set("a", 2.51234 as f32).unwrap();
+        lua.set("b", 3.4123456789 as f64).unwrap();
+
+        let x: f32 = lua.get("a").unwrap();
+        assert!(x - 2.51234 < 0.000001);
+
+        let y: f64 = lua.get("a").unwrap();
+        assert!(y - 2.51234 < 0.000001);
+
+        let z: f32 = lua.get("b").unwrap();
+        assert!(z - 3.4123456789 < 0.000001);
+
+        let w: f64 = lua.get("b").unwrap();
+        assert!(w - 3.4123456789 < 0.000001);
+    }
+
+    #[test]
+    fn readwrite_bools() {
+        let mut lua = super::super::Lua::new();
+
+        lua.set("a", true).unwrap();
+        lua.set("b", false).unwrap();
+
+        let x: bool = lua.get("a").unwrap();
+        assert_eq!(x, true);
+
+        let y: bool = lua.get("b").unwrap();
+        assert_eq!(y, false);
+    }
+
+    #[test]
+    fn readwrite_strings() {
+        let mut lua = super::super::Lua::new();
+
+        lua.set("a", "hello").unwrap();
+        lua.set("b", "hello".to_string()).unwrap();
+
+        let x: String = lua.get("a").unwrap();
+        assert_eq!(x.as_slice(), "hello");
+
+        let y: String = lua.get("b").unwrap();
+        assert_eq!(y.as_slice(), "hello");
+    }
+
+    #[test]
+    fn int_to_string() {
+        let mut lua = super::super::Lua::new();
+
+        lua.set("a", 2).unwrap();
+
+        let x: String = lua.get("a").unwrap();
+        assert_eq!(x.as_slice(), "2");
+    }
+
+    #[test]
+    fn string_to_int() {
+        let mut lua = super::super::Lua::new();
+
+        lua.set("a", "2").unwrap();
+        lua.set("b", "aaa").unwrap();
+
+        let x: int = lua.get("a").unwrap();
+        assert_eq!(x, 2);
+
+        let y: Option<int> = lua.get("b");
+        assert!(y.is_none());
     }
 }
