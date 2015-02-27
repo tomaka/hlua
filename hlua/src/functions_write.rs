80c3de6f615c988286beb1e78965878b90f63473
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-02-27 13:59:26 +0100

    Cleanup the functions_write module

--- hlua/src/functions_write.rs
+++ hlua/src/functions_write.rs
@@ -29,52 +29,6 @@ pub struct Function<F, P, R> {
     marker: PhantomData<(P, R)>,
 }
 
-// this function is the main entry point when Lua wants to call one of our functions
-extern fn wrapper1(lua: *mut ffi::lua_State) -> ::libc::c_int {
-    // we load the pointer to the wrapper2 function from an upvalue (an upvalue is a value that was pushed alongside our function)
-    let wrapper2raw = unsafe { ffi::lua_touserdata(lua, ffi::lua_upvalueindex(2)) };
-    let wrapper2: fn(*mut ffi::lua_State)->::libc::c_int = unsafe { ::std::mem::transmute(wrapper2raw) };
-
-    wrapper2(lua)
-}
-
-// this function is called when Lua wants to call one of our functions
-fn wrapper2<T, P, R>(lua: *mut ffi::lua_State) -> libc::c_int
-                     where T: FnMut<P, Output=R>,
-                           P: for<'p> LuaRead<&'p mut InsideCallback> + 'static,
-                           R: for<'p> Push<&'p mut InsideCallback>
-{
-    // loading the object that we want to call from the Lua context
-    let data_raw = unsafe { ffi::lua_touserdata(lua, ffi::lua_upvalueindex(1)) };
-    let data: &mut T = unsafe { mem::transmute(data_raw) };
-
-    // creating a temporary Lua context in order to pass it to push & read functions
-    let mut tmp_lua = InsideCallback { lua: LuaContext(lua) };
-
-    // trying to read the arguments
-    let arguments_count = unsafe { ffi::lua_gettop(lua) } as i32;
-    let args = match LuaRead::lua_read_at_position(&mut tmp_lua, -arguments_count as libc::c_int) {      // TODO: what if the user has the wrong params?
-        None => {
-            let err_msg = format!("wrong parameter types for callback function");
-            err_msg.push_to_lua(&mut tmp_lua).forget();
-            unsafe { ffi::lua_error(lua); }
-            unreachable!()
-        },
-        Some(a) => a
-    };
-
-    let ret_value = data.call_mut(args);
-
-    // pushing back the result of the function on the stack
-    let nb = ret_value.push_to_lua(&mut tmp_lua).forget();
-    nb as libc::c_int
-}
-
-// this trait should be implemented on objects that are pushed to be callbacks
-trait AnyCallable {
-    fn load_args_and_call(&mut self, lua: *mut ffi::lua_State) -> ::libc::c_int;
-}
-
 /// Opaque type that represents the Lua context when inside a callback.
 ///
 /// Some types (like `Result`) can only be returned from a callback and not written inside a
@@ -108,22 +62,18 @@ impl<L, F, P, R> Push<L> for Function<F, P, R>
               R: for<'a> Push<&'a mut InsideCallback> + 'static
 {
     fn push_to_lua(self, mut lua: L) -> PushGuard<L> {
-        // pushing the function pointer as a userdata
-        let lua_data_raw = unsafe {
-            ffi::lua_newuserdata(lua.as_mut_lua().0, mem::size_of::<F>() as libc::size_t)
-        };
+        unsafe {
+            // pushing the function pointer as a userdata
+            let lua_data = ffi::lua_newuserdata(lua.as_mut_lua().0,
+                                                mem::size_of::<F>() as libc::size_t);
+            let lua_data: *mut F = mem::transmute(lua_data);
+            ptr::write(lua_data, self.function);
 
-        let lua_data: *mut F = unsafe { mem::transmute(lua_data_raw) };
-        unsafe { ptr::write(lua_data, self.function) };
-
-        // pushing wrapper2 as a lightuserdata
-        let wrapper2: fn(*mut ffi::lua_State) -> libc::c_int = wrapper2::<F, P, R>;
-        unsafe { ffi::lua_pushlightuserdata(lua.as_mut_lua().0, mem::transmute(wrapper2)) };
-
-        // pushing wrapper1 as a closure
-        unsafe { ffi::lua_pushcclosure(lua.as_mut_lua().0, mem::transmute(wrapper1), 2) };
-
-        PushGuard { lua: lua, size: 1 }
+            // pushing wrapper as a closure
+            let wrapper: extern fn(*mut ffi::lua_State) -> libc::c_int = wrapper::<F, P, R>;
+            ffi::lua_pushcclosure(lua.as_mut_lua().0, wrapper, 1);
+            PushGuard { lua: lua, size: 1 }
+        }
     }
 }   
 
@@ -143,4 +93,36 @@ impl<'a, T, E> Push<&'a mut InsideCallback> for Result<T, E>
             }
         }
     }
+}
+
+// this function is called when Lua wants to call one of our functions
+extern fn wrapper<T, P, R>(lua: *mut ffi::lua_State) -> libc::c_int
+                           where T: FnMut<P, Output=R>,
+                                 P: for<'p> LuaRead<&'p mut InsideCallback> + 'static,
+                                 R: for<'p> Push<&'p mut InsideCallback>
+{
+    // loading the object that we want to call from the Lua context
+    let data_raw = unsafe { ffi::lua_touserdata(lua, ffi::lua_upvalueindex(1)) };
+    let data: &mut T = unsafe { mem::transmute(data_raw) };
+
+    // creating a temporary Lua context in order to pass it to push & read functions
+    let mut tmp_lua = InsideCallback { lua: LuaContext(lua) };
+
+    // trying to read the arguments
+    let arguments_count = unsafe { ffi::lua_gettop(lua) } as i32;
+    let args = match LuaRead::lua_read_at_position(&mut tmp_lua, -arguments_count as libc::c_int) {      // TODO: what if the user has the wrong params?
+        None => {
+            let err_msg = format!("wrong parameter types for callback function");
+            err_msg.push_to_lua(&mut tmp_lua).forget();
+            unsafe { ffi::lua_error(lua); }
+            unreachable!()
+        },
+        Some(a) => a
+    };
+
+    let ret_value = data.call_mut(args);
+
+    // pushing back the result of the function on the stack
+    let nb = ret_value.push_to_lua(&mut tmp_lua).forget();
+    nb as libc::c_int
 }
