790775e6ccc130bafb7c142ec5150d8f92fbe195
Greg Chapple <gregchapple1@gmail.com>
2016-02-01 13:31:22 +0000

    Remove hard-coded type from macros

--- hlua/src/macros.rs
+++ hlua/src/macros.rs
@@ -1,7 +1,7 @@
 #[macro_export]
 macro_rules! implement_lua_push {
     ($ty:ty, $cb:expr) => {
-        impl<L> $crate::Push<L> for Sound where L: $crate::AsMutLua {
+        impl<L> $crate::Push<L> for $ty where L: $crate::AsMutLua {
             fn push_to_lua(self, lua: L) -> $crate::PushGuard<L> {
                 $crate::userdata::push_userdata(self, lua, $cb)
             }
@@ -12,15 +12,15 @@ macro_rules! implement_lua_push {
 #[macro_export]
 macro_rules! implement_lua_read {
     ($ty:ty) => {
-        impl<'s, 'c> hlua::LuaRead<&'c mut hlua::InsideCallback> for &'s mut Sound {
-            fn lua_read_at_position(lua: &'c mut hlua::InsideCallback, index: i32) -> Result<&'s mut Sound, &'c mut hlua::InsideCallback> {
+        impl<'s, 'c> hlua::LuaRead<&'c mut hlua::InsideCallback> for &'s mut $ty {
+            fn lua_read_at_position(lua: &'c mut hlua::InsideCallback, index: i32) -> Result<&'s mut $ty, &'c mut hlua::InsideCallback> {
                 // FIXME: 
                 unsafe { ::std::mem::transmute($crate::userdata::read_userdata::<$ty>(lua, index)) }
             }
         }
 
-        impl<'s, 'c> hlua::LuaRead<&'c mut hlua::InsideCallback> for &'s Sound {
-            fn lua_read_at_position(lua: &'c mut hlua::InsideCallback, index: i32) -> Result<&'s Sound, &'c mut hlua::InsideCallback> {
+        impl<'s, 'c> hlua::LuaRead<&'c mut hlua::InsideCallback> for &'s $ty {
+            fn lua_read_at_position(lua: &'c mut hlua::InsideCallback, index: i32) -> Result<&'s $ty, &'c mut hlua::InsideCallback> {
                 // FIXME: 
                 unsafe { ::std::mem::transmute($crate::userdata::read_userdata::<$ty>(lua, index)) }
             }
