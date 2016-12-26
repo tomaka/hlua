9f8930f58a5741a4e6e42f3805155ee403868fe0
Pierre Krieger <pierre.krieger1708@gmail.com>
2016-12-26 14:59:12 +0100

    Finish moving most tests to src/

--- hlua/src/functions_write.rs
+++ hlua/src/functions_write.rs
@@ -268,3 +268,128 @@ extern "C" fn wrapper<T, P, R>(lua: *mut ffi::lua_State) -> libc::c_int
     };
     nb as libc::c_int
 }
+
+#[cfg(test)]
+mod tests {
+    use Lua;
+    use LuaError;
+    use function0;
+    use function1;
+    use function2;
+
+    #[test]
+    fn simple_function() {
+        let mut lua = Lua::new();
+
+        fn ret5() -> i32 {
+            5
+        };
+        lua.set("ret5", function0(ret5));
+
+        let val: i32 = lua.execute("return ret5()").unwrap();
+        assert_eq!(val, 5);
+    }
+
+    #[test]
+    fn one_argument() {
+        let mut lua = Lua::new();
+
+        fn plus_one(val: i32) -> i32 {
+            val + 1
+        };
+        lua.set("plus_one", function1(plus_one));
+
+        let val: i32 = lua.execute("return plus_one(3)").unwrap();
+        assert_eq!(val, 4);
+    }
+
+    #[test]
+    fn two_arguments() {
+        let mut lua = Lua::new();
+
+        fn add(val1: i32, val2: i32) -> i32 {
+            val1 + val2
+        };
+        lua.set("add", function2(add));
+
+        let val: i32 = lua.execute("return add(3, 7)").unwrap();
+        assert_eq!(val, 10);
+    }
+
+    #[test]
+    fn wrong_arguments_types() {
+        let mut lua = Lua::new();
+
+        fn add(val1: i32, val2: i32) -> i32 {
+            val1 + val2
+        };
+        lua.set("add", function2(add));
+
+        match lua.execute::<i32>("return add(3, \"hello\")") {
+            Err(LuaError::ExecutionError(_)) => (),
+            _ => panic!(),
+        }
+    }
+
+    #[test]
+    fn return_result() {
+        let mut lua = Lua::new();
+
+        fn always_fails() -> Result<i32, &'static str> {
+            Err("oops, problem")
+        };
+        lua.set("always_fails", function0(always_fails));
+
+        match lua.execute::<()>("always_fails()") {
+            Err(LuaError::ExecutionError(_)) => (),
+            _ => panic!(),
+        }
+    }
+
+    #[test]
+    fn closures() {
+        let mut lua = Lua::new();
+
+        lua.set("add", function2(|a: i32, b: i32| a + b));
+        lua.set("sub", function2(|a: i32, b: i32| a - b));
+
+        let val1: i32 = lua.execute("return add(3, 7)").unwrap();
+        assert_eq!(val1, 10);
+
+        let val2: i32 = lua.execute("return sub(5, 2)").unwrap();
+        assert_eq!(val2, 3);
+    }
+
+    #[test]
+    fn closures_lifetime() {
+        fn t<F>(f: F)
+            where F: Fn(i32, i32) -> i32
+        {
+            let mut lua = Lua::new();
+
+            lua.set("add", function2(f));
+
+            let val1: i32 = lua.execute("return add(3, 7)").unwrap();
+            assert_eq!(val1, 10);
+        }
+
+        t(|a, b| a + b);
+    }
+
+    #[test]
+    fn closures_extern_access() {
+        let mut a = 5;
+
+        {
+            let mut lua = Lua::new();
+
+            lua.set("inc", function0(|| a += 1));
+            for _ in 0 .. 15 {
+                lua.execute::<()>("inc()").unwrap();
+            }
+        }
+
+        assert_eq!(a, 20)
+    }
+
+}
--- hlua/src/rust_tables.rs
+++ hlua/src/rust_tables.rs
@@ -155,3 +155,77 @@ impl<'lua, L, K, E> PushOne<L> for HashSet<K>
           K: for<'a, 'b> PushOne<&'a mut &'b mut L, Err = E> + Eq + Hash
 {
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use std::collections::HashSet;
+    use Lua;
+    use LuaTable;
+
+    #[test]
+    fn write() {
+        let mut lua = Lua::new();
+
+        lua.set("a", vec![9, 8, 7]);
+
+        let mut table: LuaTable<_> = lua.get("a").unwrap();
+
+        let values: Vec<(i32, i32)> = table.iter().filter_map(|e| e).collect();
+        assert_eq!(values, vec![(1, 9), (2, 8), (3, 7)]);
+    }
+
+    #[test]
+    fn write_map() {
+        let mut lua = Lua::new();
+
+        let mut map = HashMap::new();
+        map.insert(5, 8);
+        map.insert(13, 21);
+        map.insert(34, 55);
+
+        lua.set("a", map.clone());
+
+        let mut table: LuaTable<_> = lua.get("a").unwrap();
+
+        let values: HashMap<i32, i32> = table.iter().filter_map(|e| e).collect();
+        assert_eq!(values, map);
+    }
+
+    #[test]
+    fn write_set() {
+        let mut lua = Lua::new();
+
+        let mut set = HashSet::new();
+        set.insert(5);
+        set.insert(8);
+        set.insert(13);
+        set.insert(21);
+        set.insert(34);
+        set.insert(55);
+
+        lua.set("a", set.clone());
+
+        let mut table: LuaTable<_> = lua.get("a").unwrap();
+
+        let values: HashSet<i32> = table.iter()
+            .filter_map(|e| e)
+            .map(|(elem, set): (i32, bool)| {
+                assert!(set);
+                elem
+            })
+            .collect();
+
+        assert_eq!(values, set);
+    }
+
+    #[test]
+    fn globals_table() {
+        let mut lua = Lua::new();
+
+        lua.globals_table().set("a", 12);
+
+        let val: i32 = lua.get("a").unwrap();
+        assert_eq!(val, 12);
+    }
+}
